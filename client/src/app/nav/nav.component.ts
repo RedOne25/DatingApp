334e3c345f9d441cd5c56cc7dbdb35d5483702ea
Mohammed Radhwane BENLATRECHE <benlatreche.radhwane@gmail.com>
2020-09-30 18:54:39 +0200

    End of section 5

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -8,9 +10,8 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model:any ={};
-  loggedIn: boolean;
 
-  constructor(private accountService : AccountService) { 
+  constructor(public accountService : AccountService) { 
 
   }
 
@@ -20,7 +21,6 @@ export class NavComponent implements OnInit {
   login(){
         this.accountService.login(this.model).subscribe(response =>{
           console.log(response);
-          this.loggedIn = true;
         }, error => 
         {
           console.log(console.log(error));
@@ -29,6 +29,13 @@ export class NavComponent implements OnInit {
   }
 
   logout(){
-    this.loggedIn =false;
+    this.accountService.logout();
+  }
+
+  getCurrentUser(){
+    this.accountService.currenUser$.subscribe(user =>{
+    }, error =>{
+      console.log(error);
+    });
   }
 }
