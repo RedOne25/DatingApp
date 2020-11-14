49ac5830496f4b247353c5fd6f9290ea8747134c
Mohammed Radhwane BENLATRECHE <benlatreche.radhwane@gmail.com>
2020-11-14 13:44:59 +0100

    End of section 14

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -5,28 +5,17 @@ using System.Threading.Tasks;
 
 namespace API.Helpers
 {
-    public class UserParams
+    public class UserParams: PaginationParams
+
     {
-        private const int MaxPageSize = 50;
 
-        public int PageNumber { get; set; } = 1;
+    public string CurrentUsername { get; set; }
 
-        private int _pageSize = 10;
+    public string Gender { get; set; }
 
-        public int PageSize
-        {
-            get => _pageSize;
+    public int MinAge { get; set; } = 18;
+    public int MaxAge { get; set; } = 150;
 
-            set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
-        }
-
-        public string CurrentUsername { get; set; }
-
-        public string Gender { get; set; }
-
-        public int MinAge { get; set; } = 18;
-        public int MaxAge { get; set; } = 150;
-
-        public string OrderBy { get; set; } = "LastActive";
+    public string OrderBy { get; set; } = "LastActive";
     }
 }
